b03dad67b06c3347cc07ff8afce508d9fee43ed3
Shade <anhvu_2027@depauw.edu>
2025-01-16 03:31:58 +0700

    Setup environment
    [Ticket: X]

---
+++ Assets/Scripts/VolleyballAgent.cs
@@ -0,0 +1,141 @@
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Policies;
+
+public class VolleyballAgent : Agent
+{
+    public GameObject area;
+    Rigidbody agentRb;
+    BehaviorParameters behaviorParameters;
+    public Team teamId;
+
+    // To get ball's location for observations
+    public GameObject ball;
+    Rigidbody ballRb;
+
+    VolleyballSettings volleyballSettings;
+    VolleyballEnvController envController;
+
+    // Controls jump behavior
+    float jumpingTime;
+    Vector3 jumpTargetPos;
+    Vector3 jumpStartingPos;
+    float agentRot;
+
+    public Collider[] hitGroundColliders = new Collider[3];
+    EnvironmentParameters resetParams;
+
+    void Start()
+    {
+        envController = area.GetComponent<VolleyballEnvController>();
+    }
+
+    public override void Initialize()
+    {
+        volleyballSettings = FindObjectOfType<VolleyballSettings>();
+        behaviorParameters = gameObject.GetComponent<BehaviorParameters>();
+
+        agentRb = GetComponent<Rigidbody>();
+        ballRb = ball.GetComponent<Rigidbody>();
+        
+        // for symmetry between player side
+        if (teamId == Team.Blue)
+        {
+            agentRot = -1;
+        }
+        else
+        {
+            agentRot = 1;
+        }
+    }
+
+    /// <summary>
+    /// Moves  a rigidbody towards a position smoothly.
+    /// </summary>
+    /// <param name="targetPos">Target position.</param>
+    /// <param name="rb">The rigidbody to be moved.</param>
+    /// <param name="targetVel">The velocity to target during the
+    ///  motion.</param>
+    /// <param name="maxVel">The maximum velocity posible.</param>
+    void MoveTowards(
+        Vector3 targetPos, Rigidbody rb, float targetVel, float maxVel)
+    {
+        var moveToPos = targetPos - rb.worldCenterOfMass;
+        var velocityTarget = Time.fixedDeltaTime * targetVel * moveToPos;
+        if (float.IsNaN(velocityTarget.x) == false)
+        {
+            rb.velocity = Vector3.MoveTowards(
+                rb.velocity, velocityTarget, maxVel);
+        }
+    }
+
+    /// <summary>
+    /// Check if agent is on the ground to enable/disable jumping
+    /// </summary>
+    public bool CheckIfGrounded()
+    {
+        hitGroundColliders = new Collider[3];
+        var o = gameObject;
+        Physics.OverlapBoxNonAlloc(
+            o.transform.localPosition + new Vector3(0, -0.05f, 0),
+            new Vector3(0.95f / 2f, 0.5f, 0.95f / 2f),
+            hitGroundColliders,
+            o.transform.rotation);
+        var grounded = false;
+        foreach (var col in hitGroundColliders)
+        {
+            if (col != null && col.transform != transform &&
+                (col.CompareTag("walkableSurface") ||
+                 col.CompareTag("purpleGoal") ||
+                 col.CompareTag("blueGoal")))
+            {
+                grounded = true; //then we're grounded
+                break;
+            }
+        }
+        return grounded;
+    }
+
+    /// <summary>
+    /// Called when agent collides with the ball
+    /// </summary>
+    void OnCollisionEnter(Collision c)
+    {
+        if (c.gameObject.CompareTag("ball"))
+        {
+            envController.UpdateLastHitter(teamId);
+        }
+    }
+
+    /// <summary>
+    /// Starts the jump sequence
+    /// </summary>
+    public void Jump()
+    {
+        jumpingTime = 0.2f;
+        jumpStartingPos = agentRb.position;
+    }
+
+    /// <summary>
+    /// Resolves the agent movement
+    /// </summary>
+    public void MoveAgent(ActionSegment<int> act)
+    {
+    }
+
+    public override void OnActionReceived(ActionBuffers actionBuffers)
+    {
+        MoveAgent(actionBuffers.DiscreteActions);
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+    }
+
+    // For human controller
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+    }
+}
